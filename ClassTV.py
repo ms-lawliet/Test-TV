006187070bb7c7ef96fa53f605bd35bb97b634fe
Ms Lawliet <alexieanoya@gmail.com>
2023-05-13 12:15:16 +0800

    wrote program instructions

---
+++ ClassTV.py
@@ -0,0 +1,4 @@
+# Create a Python Code for creating the Class named TV and a Test Driver program named TestTV
+# that will create two objects from Class TV and will produce the following output:
+# tv1's channel is 30 and volume level is 3
+# tv2's channel is 3 and volume level is 2
