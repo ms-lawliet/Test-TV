356e77b950c07f89ff7461957c73336fb2a90e02
Ms Lawliet <alexieanoya@gmail.com>
2023-05-14 21:54:42 +0800

    made pseudocode for test driver program, imported class from ClassTv.py file, and created objects for testing

--- TestTV.py
+++ TestTV.py
@@ -2,3 +2,10 @@
 # that will create two objects from Class TV and will produce the following output:
 # tv1's channel is 30 and volume level is 3
 # tv2's channel is 3 and volume level is 2
+
+from ClassTV import TV    # import class TV from ClassTV.py file
+
+tv1 = TV("tv1", 30, 2, "off")  # create objects
+tv2 = TV("tv2", 3, 2, "on")
+
+# call methods
