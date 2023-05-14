1de29f5357d323f7f189639f75f55d2688e96eaa
Ms Lawliet <alexieanoya@gmail.com>
2023-05-14 22:03:25 +0800

    encapsulated code in try...catch

--- TestTV.py
+++ TestTV.py
@@ -5,14 +5,18 @@
 
 from ClassTV import TV    # import class TV from ClassTV.py file
 
-name = input("Enter name of tv: ")
-channel = int(input("Choose channel from 1-30: "))
-volume_level = int(input("Choose volume level from 0-7: "))
-on_off = input("Would you like to turn on your tv? (on or off) ")
+try:
+    name = input("Enter name of tv: ")
+    channel = int(input("Choose channel from 1-30: "))
+    volume_level = int(input("Choose volume level from 0-7: "))
+    on_off = input("Would you like to turn on your tv? (on or off) ")
 
-tv1 = TV(name, channel, volume_level, on_off)  # create objects
-tv2 = TV(name, channel, volume_level, on_off)
+    tv1 = TV(name, channel, volume_level, on_off)  # create objects
+    tv2 = TV(name, channel, volume_level, on_off)
 
-# call methods
-tv1.get_channel()
-tv1.print_details()
+    # call methods
+    tv1.get_channel()
+    tv1.print_details()
+
+except ValueError:
+    print("Invalid input. Please try again.")
