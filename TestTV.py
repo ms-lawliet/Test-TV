f2f95aa792ae00aad41972a796ee5ba2db7425a6
Ms Lawliet <alexieanoya@gmail.com>
2023-05-16 14:57:21 +0800

    fixed some comments

--- TestTV.py
+++ TestTV.py
@@ -1,3 +1,5 @@
+# This is the main program
+
 # Create a Python Code for creating the Class named TV and a Test Driver program named TestTV
 # that will create two objects from Class TV and will produce the following output:
 # tv1's channel is 30 and volume level is 3
